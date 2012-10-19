77f539049fd6a0524239a80d3c777c773ee6b73b
inak <ochoak@gmail.com>
2012-10-19 19:09:11 +0200

    first comit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*
+ * Inaki lanz 
+ * Oct-2012
+ */
+
+
+$.fn.writeMail = function (username,server){
+	var e = $(this);
+	var mail = username+"@"+server;
+
+	$(e).text(mail);
+	$(e).attr("href",'mailto:'+mail);
+}
+
+$.fn.writePhone = function (prefix,number){
+	var e = $(this);
+	var phone = "(+"+prefix+") "+number;
+
+	$(e).text(phone);
+}
+
+$(function(){	
+	$("a.mail").writeMail("inaki.lanz","ochoak.com");
+	$("span.phone").writePhone("34","654756569");
+});
